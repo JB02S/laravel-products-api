1f9bb7a37eeb42602538e74dc30f21faf1129ab8
JB02S <the_real_one_7000@proton.me>
2024-10-08 08:06:06 +0100

    updating products model

--- database/migrations/2024_10_08_064523_create_products_table.php
+++ database/migrations/2024_10_08_064523_create_products_table.php
@@ -14,7 +14,10 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
-            $table->string('name');             
+            $table->string('name');
+            $table->string('price');
+            $table->json('images');
+            $table->string('description');
         });
     }
 
