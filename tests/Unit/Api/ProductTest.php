013766439fcaa93a1b48b62c84786440894ae4a2
JB02S <the_real_one_7000@proton.me>
2024-10-11 10:43:39 +0100

    update cd pipeline

---
+++ tests/Unit/Api/ProductTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Tests\Unit\Api;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use PHPUnit\Framework\TestCase;
+
+class ProductTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_example(): void
+    {
+        $this->assertTrue(true);
+    }
+}
